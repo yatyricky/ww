ec9f8b6b764f4fe8e952613a545b403dae71fefe
Nefinity <yatyricky@gmail.com>
2017-05-09 15:48:37 +0800

    added rest chars

--- js/characters.js
+++ js/characters.js
@@ -1,92 +1,297 @@
 var characters = {
-    "vl": {
+    "aa": {
         "name": "普通村民",
         "img": "imgs/townfolk.jpg",
-        "sort": 1
+        "sort": 1000
     },
-    "st": {
-        "name": "高級村民",
-        "img": "imgs/seniortownfolk.jpg",
-        "sort": 1
-    },
-    "sr": {
-        "name": "預言家",
-        "img": "imgs/seer.jpg",
-        "sort": 100
-    },
-    "wt": {
-        "name": "女巫",
-        "img": "imgs/witch.jpg",
-        "sort": 101
-    },
-    "ht": {
-        "name": "獵人",
-        "img": "imgs/hunter.jpg",
-        "sort": 102
-    },
-    "gd": {
-        "name": "守衛",
-        "img": "imgs/guard.jpg",
-        "sort": 102
-    },
-    "fl": {
-        "name": "白癡",
-        "img": "imgs/fool.jpg",
-        "sort": 102
-    },
-    "cp": {
-        "name": "丘比特",
-        "img": "imgs/cupid.jpg",
-        "sort": 103
-    },
-    "tf": {
-        "name": "盜賊",
-        "img": "imgs/thief.jpg",
-        "sort": 103
-    },
-    "me": {
-        "name": "禁言長老",
-        "img": "imgs/mutingelder.jpg",
-        "sort": 104
-    },
-    "rk": {
-        "name": "銹劍騎士",
-        "img": "imgs/rustsword.jpg",
-        "sort": 104
-    },
-    "fx": {
-        "name": "狐狸",
-        "img": "imgs/fox.jpg",
-        "sort": 105
-    },
-    "br": {
-        "name": "馴熊師",
-        "img": "imgs/bear.jpg",
-        "sort": 105
-    },
-    "wc": {
-        "name": "野孩子",
-        "img": "imgs/wildchild.jpg",
-        "sort": 104
-    },
-    "sg": {
-        "name": "替罪羊",
-        "img": "imgs/scapegoat.jpg",
-        "sort": 103
-    },
-    "ww": {
+    "ab": {
         "name": "狼人",
         "img": "imgs/werewolf.jpg",
-        "sort": 250
+        "sort": 1100
     },
-    "wh": {
+    "ac": {
+        "name": "預言家",
+        "img": "imgs/seer.jpg",
+        "sort": 1200
+    },
+    "ad": {
+        "name": "女巫",
+        "img": "imgs/witch.jpg",
+        "sort": 1201
+    },
+    "ae": {
+        "name": "獵人",
+        "img": "imgs/hunter.jpg",
+        "sort": 1202
+    },
+    "af": {
+        "name": "丘比特",
+        "img": "imgs/cupid.jpg",
+        "sort": 1203
+    },
+    "ag": {
+        "name": "盜賊",
+        "img": "imgs/thief.jpg",
+        "sort": 1204
+    },
+    "ah": {
+        "name": "小女孩",
+        "img": "imgs/littlegirl.jpg",
+        "sort": 1205
+    },
+    "ai": {
+        "name": "守衛",
+        "img": "imgs/guard.jpg",
+        "sort": 2200
+    },
+    "aj": {
+        "name": "長老",
+        "img": "imgs/elder.jpg",
+        "sort": 2201
+    },
+    "ak": {
+        "name": "白癡",
+        "img": "imgs/fool.jpg",
+        "sort": 2202
+    },
+    "al": {
+        "name": "替罪羊",
+        "img": "imgs/scapegoat.jpg",
+        "sort": 2203
+    },
+    "am": {
+        "name": "吹笛者",
+        "img": "imgs/flutemancer.jpg",
+        "sort": 2204
+    },
+    "an": {
         "name": "白狼王",
         "img": "imgs/whitewolf.jpg",
-        "sort": 200
+        "sort": 2100
     },
-    "dm": {
+    "ao": {
+        "name": "乌鸦",
+        "img": "imgs/crow.jpg",
+        "sort": 2205
+    },
+    "ap": {
+        "name": "縱火者",
+        "img": "imgs/arsonist.jpg",
+        "sort": 2206
+    },
+    "aq": {
+        "name": "狼狗",
+        "img": "imgs/wolfdog.jpg",
+        "sort": 3100
+    },
+    "ar": {
+        "name": "大野狼",
+        "img": "imgs/bigbadwolf.jpg",
+        "sort": 3101
+    },
+    "as": {
+        "name": "種狼",
+        "img": "imgs/infector.jpg",
+        "sort": 3102
+    },
+    "at": {
+        "name": "野孩子",
+        "img": "imgs/wildchild.jpg",
+        "sort": 3103
+    },
+    "au": {
+        "name": "高級村民",
+        "img": "imgs/seniortownfolk.jpg",
+        "sort": 3000
+    },
+    "av": {
+        "name": "兩姊妹",
+        "img": "imgs/twosisters.jpg",
+        "sort": 3200
+    },
+    "aw": {
+        "name": "三兄弟",
+        "img": "imgs/threebrothers.jpg",
+        "sort": 3201
+    },
+    "ax": {
+        "name": "狐狸",
+        "img": "imgs/fox.jpg",
+        "sort": 3202
+    },
+    "ay": {
+        "name": "口吃的法官",
+        "img": "imgs/falteringjudge.jpg",
+        "sort": 3203
+    },
+    "az": {
+        "name": "吉普賽娘",
+        "img": "imgs/gypsymaid.jpg",
+        "sort": 3204
+    },
+    "ba": {
+        "name": "馴熊師",
+        "img": "imgs/bear.jpg",
+        "sort": 3205
+    },
+    "bb": {
+        "name": "銹劍騎士",
+        "img": "imgs/rustsword.jpg",
+        "sort": 3206
+    },
+    "bc": {
+        "name": "順從的女僕",
+        "img": "imgs/obeyingmaid.jpg",
+        "sort": 3207
+    },
+    "bd": {
+        "name": "天使",
+        "img": "imgs/angel.jpg",
+        "sort": 3208
+    },
+    "be": {
+        "name": "邪惡的宗族老爺爺",
+        "img": "imgs/snisterpapa.jpg",
+        "sort": 3209
+    },
+    "bf": {
+        "name": "變臉者",
+        "img": "imgs/faceturner.jpg",
+        "sort": 3210
+    },
+    "bg": {
+        "name": "九尾狐",
+        "img": "imgs/neptailfox.jpg",
+        "sort": 3211
+    },
+    "bh": {
+        "name": "藥劑師",
+        "img": "imgs/alchemist.jpg",
+        "sort": 4200
+    },
+    "bi": {
+        "name": "刺客",
+        "img": "imgs/assassin.jpg",
+        "sort": 4201
+    },
+    "bj": {
+        "name": "蠱惑師",
+        "img": "imgs/hypnizer.jpg",
+        "sort": 4202
+    },
+    "bk": {
+        "name": "操縱月亮的女孩",
+        "img": "imgs/moonmanipulator.jpg",
+        "sort": 4203
+    },
+    "bl": {
+        "name": "騎士",
+        "img": "imgs/knight.jpg",
+        "sort": 4204
+    },
+    "bm": {
+        "name": "狼兄弟",
+        "img": "imgs/wolfbrothers.jpg",
+        "sort": 4100
+    },
+    "bn": {
+        "name": "火狼",
+        "img": "imgs/firewolf.jpg",
+        "sort": 4101
+    },
+    "bo": {
+        "name": "復仇者",
+        "img": "imgs/revenger.jpg",
+        "sort": 4102
+    },
+    "bp": {
+        "name": "影子",
+        "img": "imgs/shadow.jpg",
+        "sort": 4103
+    },
+    "bq": {
+        "name": "通靈人",
+        "img": "imgs/necromancer.jpg",
+        "sort": 5200
+    },
+    "br": {
+        "name": "公爵",
+        "img": "imgs/duke.jpg",
+        "sort": 5201
+    },
+    "bs": {
+        "name": "旅者",
+        "img": "imgs/traveller.jpg",
+        "sort": 5202
+    },
+    "bt": {
+        "name": "聖女",
+        "img": "imgs/holymaid.jpg",
+        "sort": 6200
+    },
+    "bu": {
+        "name": "麵包師",
+        "img": "imgs/baker.jpg",
+        "sort": 6201
+    },
+    "bv": {
+        "name": "女巫的學徒",
+        "img": "imgs/witchapprentice.jpg",
+        "sort": 6202
+    },
+    "bw": {
+        "name": "賞金獵人",
+        "img": "imgs/bountyhunter.jpg",
+        "sort": 6203
+    },
+    "bx": {
+        "name": "聖靈弓手",
+        "img": "imgs/holyarcher.jpg",
+        "sort": 6204
+    },
+    "by": {
+        "name": "太陽",
+        "img": "imgs/sun.jpg",
+        "sort": 6205
+    },
+    "bz": {
+        "name": "月亮",
+        "img": "imgs/moon.jpg",
+        "sort": 6206
+    },
+    "ca": {
+        "name": "術士",
+        "img": "imgs/warlock.jpg",
+        "sort": 6207
+    },
+    "cb": {
+        "name": "雙生兒",
+        "img": "imgs/conjoined.jpg",
+        "sort": 6208
+    },
+    "cc": {
+        "name": "狼美人",
+        "img": "imgs/beautywolf.jpg",
+        "sort": 9100
+    },
+    "cd": {
+        "name": "潛行者",
+        "img": "imgs/stealther.jpg",
+        "sort": 9200
+    },
+    "ce": {
+        "name": "禁言長老",
+        "img": "imgs/mutingelder.jpg",
+        "sort": 9201
+    },
+    "cf": {
+        "name": "魅魔",
+        "img": "imgs/succubus.jpg",
+        "sort": 9202
+    },
+    "cg": {
         "name": "惡魔",
         "img": "imgs/demon.jpg",
-        "sort": 200
+        "sort": 9101
     }
-}
+};
