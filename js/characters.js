8e222e23c82a47f48ff21dbf6a124121907af697
Rick Sun <yatyricky@gmail.com>
2019-01-14 14:05:40 +0800

    move imgs to cdn

--- js/characters.js
+++ js/characters.js
@@ -1,297 +1,297 @@
 var characters = {
     "aa": {
         "name": "普通村民",
-        "img": "imgs/townfolk.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/townfolk.jpg",
         "sort": 1000
     },
     "ab": {
         "name": "狼人",
-        "img": "imgs/werewolf.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/werewolf.jpg",
         "sort": 1100
     },
     "ac": {
         "name": "預言家",
-        "img": "imgs/seer.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/seer.jpg",
         "sort": 1200
     },
     "ad": {
         "name": "女巫",
-        "img": "imgs/witch.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/witch.jpg",
         "sort": 1201
     },
     "ae": {
         "name": "獵人",
-        "img": "imgs/hunter.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/hunter.jpg",
         "sort": 1202
     },
     "af": {
         "name": "丘比特",
-        "img": "imgs/cupid.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/cupid.jpg",
         "sort": 1203
     },
     "ag": {
         "name": "盜賊",
-        "img": "imgs/thief.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/thief.jpg",
         "sort": 1204
     },
     "ah": {
         "name": "小女孩",
-        "img": "imgs/littlegirl.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/littlegirl.jpg",
         "sort": 1205
     },
     "ai": {
         "name": "守衛",
-        "img": "imgs/guard.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/guard.jpg",
         "sort": 2200
     },
     "aj": {
         "name": "長老",
-        "img": "imgs/elder.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/elder.jpg",
         "sort": 2201
     },
     "ak": {
         "name": "白癡",
-        "img": "imgs/fool.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/fool.jpg",
         "sort": 2202
     },
     "al": {
         "name": "替罪羊",
-        "img": "imgs/scapegoat.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/scapegoat.jpg",
         "sort": 2203
     },
     "am": {
         "name": "吹笛者",
-        "img": "imgs/flutemancer.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/flutemancer.jpg",
         "sort": 2204
     },
     "an": {
         "name": "白狼王",
-        "img": "imgs/whitewolf.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/whitewolf.jpg",
         "sort": 2100
     },
     "ao": {
         "name": "乌鸦",
-        "img": "imgs/crow.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/crow.jpg",
         "sort": 2205
     },
     "ap": {
         "name": "縱火者",
-        "img": "imgs/arsonist.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/arsonist.jpg",
         "sort": 2206
     },
     "aq": {
         "name": "狼狗",
-        "img": "imgs/wolfdog.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/wolfdog.jpg",
         "sort": 3100
     },
     "ar": {
         "name": "大野狼",
-        "img": "imgs/bigbadwolf.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/bigbadwolf.jpg",
         "sort": 3101
     },
     "as": {
         "name": "種狼",
-        "img": "imgs/infector.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/infector.jpg",
         "sort": 3102
     },
     "at": {
         "name": "野孩子",
-        "img": "imgs/wildchild.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/wildchild.jpg",
         "sort": 3103
     },
     "au": {
         "name": "高級村民",
-        "img": "imgs/seniortownfolk.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/seniortownfolk.jpg",
         "sort": 3000
     },
     "av": {
         "name": "兩姊妹",
-        "img": "imgs/twosisters.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/twosisters.jpg",
         "sort": 3200
     },
     "aw": {
         "name": "三兄弟",
-        "img": "imgs/threebrothers.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/threebrothers.jpg",
         "sort": 3201
     },
     "ax": {
         "name": "狐狸",
-        "img": "imgs/fox.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/fox.jpg",
         "sort": 3202
     },
     "ay": {
         "name": "口吃的法官",
-        "img": "imgs/falteringjudge.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/falteringjudge.jpg",
         "sort": 3203
     },
     "az": {
         "name": "吉普賽娘",
-        "img": "imgs/gypsymaid.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/gypsymaid.jpg",
         "sort": 3204
     },
     "ba": {
         "name": "馴熊師",
-        "img": "imgs/bear.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/bear.jpg",
         "sort": 3205
     },
     "bb": {
         "name": "銹劍騎士",
-        "img": "imgs/rustsword.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/rustsword.jpg",
         "sort": 3206
     },
     "bc": {
         "name": "順從的女僕",
-        "img": "imgs/obeyingmaid.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/obeyingmaid.jpg",
         "sort": 3207
     },
     "bd": {
         "name": "天使",
-        "img": "imgs/angel.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/angel.jpg",
         "sort": 3208
     },
     "be": {
         "name": "邪惡的宗族老爺爺",
-        "img": "imgs/snisterpapa.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/snisterpapa.jpg",
         "sort": 3209
     },
     "bf": {
         "name": "變臉者",
-        "img": "imgs/faceturner.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/faceturner.jpg",
         "sort": 3210
     },
     "bg": {
         "name": "九尾狐",
-        "img": "imgs/neptailfox.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/neptailfox.jpg",
         "sort": 3211
     },
     "bh": {
         "name": "藥劑師",
-        "img": "imgs/alchemist.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/alchemist.jpg",
         "sort": 4200
     },
     "bi": {
         "name": "刺客",
-        "img": "imgs/assassin.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/assassin.jpg",
         "sort": 4201
     },
     "bj": {
         "name": "蠱惑師",
-        "img": "imgs/hypnizer.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/hypnizer.jpg",
         "sort": 4202
     },
     "bk": {
         "name": "操縱月亮的女孩",
-        "img": "imgs/moonmanipulator.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/moonmanipulator.jpg",
         "sort": 4203
     },
     "bl": {
         "name": "騎士",
-        "img": "imgs/knight.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/knight.jpg",
         "sort": 4204
     },
     "bm": {
         "name": "狼兄弟",
-        "img": "imgs/wolfbrothers.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/wolfbrothers.jpg",
         "sort": 4100
     },
     "bn": {
         "name": "火狼",
-        "img": "imgs/firewolf.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/firewolf.jpg",
         "sort": 4101
     },
     "bo": {
         "name": "復仇者",
-        "img": "imgs/revenger.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/revenger.jpg",
         "sort": 4102
     },
     "bp": {
         "name": "影子",
-        "img": "imgs/shadow.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/shadow.jpg",
         "sort": 4103
     },
     "bq": {
         "name": "通靈人",
-        "img": "imgs/necromancer.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/necromancer.jpg",
         "sort": 5200
     },
     "br": {
         "name": "公爵",
-        "img": "imgs/duke.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/duke.jpg",
         "sort": 5201
     },
     "bs": {
         "name": "旅者",
-        "img": "imgs/traveller.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/traveller.jpg",
         "sort": 5202
     },
     "bt": {
         "name": "聖女",
-        "img": "imgs/holymaid.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/holymaid.jpg",
         "sort": 6200
     },
     "bu": {
         "name": "麵包師",
-        "img": "imgs/baker.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/baker.jpg",
         "sort": 6201
     },
     "bv": {
         "name": "女巫的學徒",
-        "img": "imgs/witchapprentice.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/witchapprentice.jpg",
         "sort": 6202
     },
     "bw": {
         "name": "賞金獵人",
-        "img": "imgs/bountyhunter.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/bountyhunter.jpg",
         "sort": 6203
     },
     "bx": {
         "name": "聖靈弓手",
-        "img": "imgs/holyarcher.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/holyarcher.jpg",
         "sort": 6204
     },
     "by": {
         "name": "太陽",
-        "img": "imgs/sun.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/sun.jpg",
         "sort": 6205
     },
     "bz": {
         "name": "月亮",
-        "img": "imgs/moon.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/moon.jpg",
         "sort": 6206
     },
     "ca": {
         "name": "術士",
-        "img": "imgs/warlock.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/warlock.jpg",
         "sort": 6207
     },
     "cb": {
         "name": "雙生兒",
-        "img": "imgs/conjoined.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/conjoined.jpg",
         "sort": 6208
     },
     "cc": {
         "name": "狼美人",
-        "img": "imgs/beautywolf.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/beautywolf.jpg",
         "sort": 9100
     },
     "cd": {
         "name": "潛行者",
-        "img": "imgs/stealther.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/stealther.jpg",
         "sort": 9200
     },
     "ce": {
         "name": "禁言長老",
-        "img": "imgs/mutingelder.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/mutingelder.jpg",
         "sort": 9201
     },
     "cf": {
         "name": "魅魔",
-        "img": "imgs/succubus.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/succubus.jpg",
         "sort": 9202
     },
     "cg": {
         "name": "惡魔",
-        "img": "imgs/demon.jpg",
+        "img": "http://plb36hyi5.bkt.clouddn.com/demon.jpg",
         "sort": 9101
     }
 };
